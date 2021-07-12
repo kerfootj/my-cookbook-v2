e471dc7b4d5833479b7066400888a7ad9e868490
Joel Kerfoot <joelkerfoot@gmail.com>
2021-07-11 17:30:36 -0700

    feature: configure eslint/prettier for frontend and graphql

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,37 +1,36 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    env: {
+        browser: true,
+        es2021: true,
     },
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-  ],
-  settings: {
-    'import/resolver': {
-      node: {
-        paths: ['src'],
-        extensions: ['.ts', '.tsx'],
-      },
+    extends: [
+        'plugin:react/recommended',
+        'airbnb',
+        'plugin:prettier/recommended',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: 'module',
     },
-  },
-  rules: {
-    '@typescript-eslint/no-use-before-define': ['error'],
-    'import/extensions': 'off',
-    'import/prefer-default-export': 'off',
-    'no-use-before-define': 'off',
-    'react/jsx-filename-extension': ['warn', { extensions: ['.tsx'] }],
-  },
+    plugins: ['react', '@typescript-eslint'],
+    settings: {
+        'import/resolver': {
+            node: {
+                paths: ['src'],
+                extensions: ['.ts', '.tsx'],
+            },
+        },
+    },
+    rules: {
+        '@typescript-eslint/no-use-before-define': ['error', {}],
+        camelcase: 'off',
+        'import/extensions': 'off',
+        'no-use-before-define': 'off',
+        'no-unused-vars': 'warn',
+        'react/jsx-filename-extension': ['warn', { extensions: ['.tsx'] }],
+    },
 };
